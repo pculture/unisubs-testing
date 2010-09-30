78564576759a8891f6148ae274339c0354faf2dd
janet <jed@pculture.org>
2010-10-01 01:48:57 +0200

    comments tests

---
+++ comments_text.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# text for comments testing
+
+normal_comment_text = u'Charles Robert Darwin FRS was an English naturalist who established that all species \
+of life have descended over time from common ancestors, and proposed the scientific theory that this branching pattern of evolution \
+resulted from a process that he called natural selection. He published his theory with compelling evidence for evolution \
+in his 1859 book On the Origin of Species'
+
+hr_comment_text = u'Darwin je autor prve moderne teorije o evoluciji živih bića pomoću koncepta razvijanja svih životnih'
+
+jp_comment_text = u'Charles Darwinという項目を新規作成する。または執筆依'
+
+ru_comment_text = u'Чарльз Ро́берт Да́рвин англ. 12 февраля 1809 — 19 апреля 1882'
+
