b653e90e587a8920947e2b3b7e130c151e54b88e
janet <jed@pculture.org>
2010-11-11 10:32:52 +0100

    youtube query

--- offsite.py
+++ offsite.py
@@ -141,9 +141,11 @@ def get_youtube_video_url(self,vid_format="embed"):
         if vid_format == "webm":
             vsel.open("results?uploaded=w&search_query=crazy&search_duration=short&webm=1&search_type=videos&uni=3&search_sort=video_date_uploaded")
         else:
-            vsel.open("results?uploaded=w&search_query=crazy&search_duration=short&search_type=videos&uni=3&search_sort=video_date_uploaded")
+            vsel.open("videos?s=mr&c=1")
+            #vsel.open("results?uploaded=w&search_query=crazy&search_duration=short&search_type=videos&uni=3&search_sort=video_date_uploaded")
         vsel.wait_for_page_to_load(testvars.MSTestVariables["TimeOut"])
-        vsel.click("css=h3 a[id^='video-long-title']")
+#        vsel.click("css=h3 a[id^='video-long-title']")
+        vsel.click("css=div.video-entry a[id^=video-thumb]")             
         vsel.wait_for_page_to_load(testvars.MSTestVariables["TimeOut"])
         youtubeURL = vsel.get_eval("window.location")
         print youtubeURL      
