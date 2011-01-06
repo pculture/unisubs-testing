ce90b7767649e86dd824f584219780d3e730132f
janet <jed@pculture.org>
2011-01-06 18:29:42 +0100

    value en default

--- website.py
+++ website.py
@@ -107,7 +107,7 @@ def front_page_submit(self,sel,url):
     sel.click("css=.btn_submit_video")
 
 
-def start_sub_widget(self,sel,wig_menu=testvars.WebsiteUI["SubtitleMe_menu"],skip="True",vid_lang="English",sub_lang="English"):
+def start_sub_widget(self,sel,wig_menu=testvars.WebsiteUI["SubtitleMe_menu"],skip="True",vid_lang="en",sub_lang="en"):
     """Start the Subtitle Widget using the Subtitle Me menu.
 
     This will handle the language choice for demo or submitted videos.
