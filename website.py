6833928a9e0ca643f3a510110ef51adb28cf3e21
janet <jed@pculture.org>
2011-03-01 16:55:58 +0100

    front-page submit

--- website.py
+++ website.py
@@ -105,7 +105,6 @@ def submit_video(self,sel,url):
 def front_page_submit(self,sel,url):
     sel.open("/")
     sel.type("css=.submit_video", url)
-    sel.type_keys("css=.submit_video",' ')
     sel.click("css=.btn_submit_video")
     sel.wait_for_page_to_load(testvars.timeout)
 
