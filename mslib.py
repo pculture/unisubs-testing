d495f76e243d519673e5bde835bc86b49c247e3e
janet <jed@pculture.org>
2011-03-01 23:30:33 +0100

    buffercrap

--- mslib.py
+++ mslib.py
@@ -40,11 +40,15 @@ def wait_for_video_to_buffer(self,sel):
 #    if selvars.set_browser() == "*firefox":
     
     try:
-        sel.click_at(testvars.WidgetUI["Play_pause"],"")
-        if sel.is_element_present(testvars.WidgetUI["Video_elapsed_time"]):
-            time.sleep(2)
-            print sel.get_text(testvars.WidgetUI["Video_elapsed_time"])
+        if sel.is_element_present(testvars.WidgetUI["Video_pause_button"]):
+            print "autoplaying"
             sel.click_at(testvars.WidgetUI["Play_pause"],"")
+        else:
+            sel.click_at(testvars.WidgetUI["Play_pause"],"")
+            if sel.is_element_present(testvars.WidgetUI["Video_elapsed_time"]):
+                time.sleep(2)
+                print sel.get_text(testvars.WidgetUI["Video_elapsed_time"])
+                sel.click_at(testvars.WidgetUI["Play_pause"],"")
         if sel.is_element_present("css=.mirosubs-buffered"):
             print " - waiting for video to buffer " +time.strftime("%M:%S", time.gmtime())
             for i in range(10):
