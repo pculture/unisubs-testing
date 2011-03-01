ecd9e30298c49657c1cd8918e13a12305633afb2
janet <jed@pculture.org>
2011-03-01 15:37:58 +0100

    testtest

--- testtest.py
+++ testtest.py
@@ -22,25 +22,35 @@ class subgroup_test(unittest.TestCase):
     def setUp(self):
         self.verificationErrors = []
         testid = self.id()
-        self.selenium = selenium(selvars.set_localhost(), selvars.set_port(), selvars.set_browser(self.id(),self.shortDescription()), "http://staging.universalsubtitles.org" )
+        self.selenium = selenium(selvars.set_localhost(), selvars.set_port(), "safari", "http://staging.universalsubtitles.org" )
         self.selenium.start()
         self.session = self.selenium.sessionId
 
 
-    def test_close(self):
+    def test_388(self):
+        """Widget Step 2, edit subtitle text from step 1.
         
-        """Widget Step 3, click time arrows to modify time.
-        
-        http://litmus.pculture.org/show_test.cgi?id=414
+        Tests smalls, caps, spaces, and non-ascii chars
+        http://litmus.pculture.org/show_test.cgi?id=388
         """
-        print "starting testcase 414"
+        print "starting testcase 388"
         sel = self.selenium
-        sel.set_timeout(180000)
+        sel.set_timeout(testvars.MSTestVariables["TimeOut"])
+        subtextfile = os.path.join(testvars.MSTestVariables["DataDirectory"],"switch-to-firefox.txt")
         # be sure logged out
-        sel.open("/videos/teams")
-        vid_url = offsite.get_youtube_video_url(self)
-        
-  
+        website.SiteLogout(self,sel)
+        website.start_demo(self,sel)
+        website.start_sub_widget(self,sel)
+        widget.transcribe_video(self, sel, subtextfile)
+        widget.sync_video(self,sel,subtextfile,step="Stop")
+                       
+        #edit subtitles
+        print "verifying subtitles are still present"
+        widget.edit_text(self,sel,subtextfile)
+        # make it french
+##        widget.edit_text(self,sel,subtextfile,new_text=testvars.eels_fr)
+##        #make it japanese
+##        widget.edit_text(self,sel,subtextfile,new_text=testvars.eels_jp)  
           
 # Close the browser, log errors, perform cleanup 
     def tearDown(self):
@@ -50,7 +60,7 @@ class subgroup_test(unittest.TestCase):
         #Close the browser
         self.selenium.sessionId = self.session
         print self.selenium.sessionId
-        self.selenium.stop()
+ #       self.selenium.stop()
         #Log any errors
         self.assertEqual([], self.verificationErrors) 
 
