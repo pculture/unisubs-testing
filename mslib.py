311129cf0c9a9d381312c8c28e9d4e0082b481a5
janet <jed@pculture.org>
2011-04-01 18:26:47 +0200

    show wait time if more than 2 seconds

--- mslib.py
+++ mslib.py
@@ -33,20 +33,25 @@ def wait_for_element_present(self,sel,input_field):
         ct += ct
     else:
         self.fail("time out waiting 30s for element " +input_field)
-    print ct
+    if ct > 2:
+        print ct
 
 def wait_for_text_present(self,sel,ttext):
     """
     Description: Waits 60 seconds for element to present itself.
     Requires: valid element identifier, can be css, xpath
     """
+    ct = 1
     for i in range(30):
         try:
             if sel.is_text_present(ttext): break
         except: pass
         time.sleep(1)
+        ct += ct
     else:
         self.fail("time out waiting 30s for text " +ttext)
+    if ct > 2:
+        print ct
 
 def wait_for_video_to_buffer(self,sel):
     """
