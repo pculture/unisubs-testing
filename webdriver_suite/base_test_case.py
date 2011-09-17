470d2b00a38b913898024290e5784e22e36b873c
janet <jed@pculture.org>
2011-09-17 22:59:03 +0200

    quit webdriver at the end of the test.

--- webdriver_suite/base_test_case.py
+++ webdriver_suite/base_test_case.py
@@ -1,6 +1,7 @@
 import unittest
 import logging
 from selenium import webdriver
+import testsetup
 
 class BaseTestCase(unittest.TestCase):       
 
@@ -10,5 +11,6 @@ class BaseTestCase(unittest.TestCase):
       
     def tearDown(self):
         print " - completed: ",self.id()
+        testsetup.browser.quit()
         
         
