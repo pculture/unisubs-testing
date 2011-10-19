c73d4385f23cbdaaca4cbfdf34a87b1dbe297e23
janet <jed@pculture.org>
2011-10-19 09:05:06 +0200

    move the webdriver commands back to html_fragment

--- webdriver_suite/html/html_fragment.py
+++ webdriver_suite/html/html_fragment.py
@@ -1,46 +1,5 @@
 #!/usr/bin/env python
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is Mozilla Firefox Support.
-#
-# The Initial Developer of the Original Code is
-# Mozilla Corp.
-# Portions created by the Initial Developer are Copyright (C) 2011
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s): Vishal
-#                 Dave Hunt <dhunt@mozilla.com>
-#                 David Burns
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
 
-'''
-Modified for Universal Subtitles Sept 15, 2011
-Created on Jun 21, 2010
-'''
 import re
 import time
 import base64
@@ -189,6 +148,33 @@ class HtmlFragment(object):
         else:
             self.record_error()
             raise Exception(element + ' has not disappeared')
+
+
+    def get_absolute_url(self, url):
+        if url.startswith("http"):
+            full_url = url
+        else:
+            full_url = self.base_url + url
+        return full_url
+   
+    def open_page(self, url):
+        self.browser.get(self.get_absolute_url(url))
+
+
+    def go_back(self):
+        self.browser.back()
+
+    def page_down(self, elements):
+        """elements are a list not a single element to try to page down.
+
+        """
+        for x in elements:
+            if self.is_element_present(x):
+                elem = self.browser.find_element_by_css_selector(x)
+                break        
+        elem.send_keys("PAGE_DOWN")
+
+
 
     def record_error(self):
         """
--- webdriver_suite/html/page.py
+++ webdriver_suite/html/page.py
@@ -13,27 +13,3 @@ class Page(HtmlFragment):
                 raise Exception('Expected page title does not match actual page title.')
         else:
             return True
-
-    def get_absolute_url(self, url):
-        if url.startswith("http"):
-            full_url = url
-        else:
-            full_url = self.base_url + url
-        return full_url
-   
-    def open_page(self, url):
-        self.browser.get(self.get_absolute_url(url))
-
-
-    def go_back(self):
-        self.browser.back()
-
-    def page_down(self, elements):
-        """elements are a list not a single element to try to page down.
-
-        """
-        for x in elements:
-            if self.is_element_present(x):
-                elem = self.browser.find_element_by_css_selector(x)
-                break        
-        elem.send_keys("PAGE_DOWN")
