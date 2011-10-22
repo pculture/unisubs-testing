25d2c92e5b7de83833825851e41d927b1fb784c0
janet <jed@pculture.org>
2011-10-22 14:10:36 +0200

    playback tests grab screenshot on error

--- webdriver_suite/test_blog_youtube_pagedemo.py
+++ webdriver_suite/test_blog_youtube_pagedemo.py
@@ -5,24 +5,24 @@ from base_test_case import BaseTestCase
 from html.offsite_page import OffsitePage
 
 
-class TestNYTimesPagedemoPage(BaseTestCase):
+class TestBlogYouTubePage(BaseTestCase):
     """NY Times pagedemo page tests.
     
     """
-    _URL = "pagedemo/nytimes_youtube_embed"
+    _URL = "pagedemo/blog_youtube_embed"
     _SUBS_WIDGET = "div.entry-content span.unisubs-widget"
 
     def test_nytimes_sub_playback_position(self):
-        """Open NY Times page, start playback, pause when subs appear and verify correct position.
+        """Open YouTube Blog Page, start playback, pause when subs appear and verify correct position.
         
         """
         try:
-            nyt_pg = OffsitePage()
-            nyt_pg.open_page(self._URL)
-            nyt_pg.pause_playback_when_subs_appear(0)
-            self.assertTrue(nyt_pg.displays_subs_in_correct_position)
+            yt_pg = OffsitePage()
+            yt_pg.open_page(self._URL)
+            yt_pg.pause_playback_when_subs_appear(0)
+            self.assertTrue(yt_pg.displays_subs_in_correct_position)
         except:
-            nyt_pg.record_error()
+            yt_pg.record_error()
         
 
 if __name__ == "__main__":
