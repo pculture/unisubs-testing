26b8e53c9e6dd6a3bef626ea2228d1bfcbb48b5a
Janet <jed@pculture>
2011-12-27 00:12:58 +0100

    adding some prints

--- tests/features/api_check_steps.py
+++ tests/features/api_check_steps.py
@@ -3,11 +3,12 @@ from lettuce import *
 
 @step('Subtitles are present for each language in the dir "(.*?)"')
 def verify_team_video_subs(self, sub_dir):
+    print 'checking the subs for this dir: % s' % sub_dir
     world.video_pg.open_original_lang()
     langs = world.data.list_of_translations(sub_dir)
     for sub_file, lang in langs:
-        print sub_file, lang
         world.video_pg.open_translation(lang)
+        print 'checking this lang %s' % lang
         subtitles = world.data.pull_sub_strings_from_data(sub_file)
         world.video_pg.verify_sub_content(subtitles)
         world.browser.back()
